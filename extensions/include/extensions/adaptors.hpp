a245149e88dabe2eeadb3e6b05281a8ac5ab5a33
Mateusz <mateusz.snamina@gmail.com>
2019-12-15 21:45:02 +0100

    refinement

--- extensions/include/extensions/adaptors.hpp
+++ extensions/include/extensions/adaptors.hpp
@@ -66,19 +66,61 @@ static Doubler doubled{};
 #pragma GCC diagnostic pop
 
 template <typename ForwardRange>
-using DoubledRangeType = ::boost::joined_range<
-    const typename ::boost::iterator_range<typename ::boost::range_iterator<const ForwardRange>::type>,
-    const typename ::boost::iterator_range<typename ::boost::range_iterator<const ForwardRange>::type>>;
+using DoubledRangeType = ::boost::joined_range<const ForwardRange, const ForwardRange>;
 
 template <typename ForwardRange>
 DoubledRangeType<ForwardRange>
 operator|(const ForwardRange &rng, const Doubler &) {
-    return ::boost::join(
-        ::boost::make_iterator_range(std::begin(rng), std::end(rng)),
-        ::boost::make_iterator_range(std::begin(rng), std::end(rng)));
+    return ::boost::join(rng, rng);
 }
 
 }  // namespace extension_implementation::boost::adaptors
+
+// #######################################################################
+// ##  refined                                                          ##
+// #######################################################################
+
+// namespace extension_implementation::boost::adaptors {
+
+// template<typename T>
+// class RefinedHolder {
+//    public:
+//     RefinedHolder(size_t n, T v) : n(n), v(v){};
+//     size_t n;
+//     T[1] v;
+// };
+
+// inline RefinedHolder refined(size_t n) {
+//     return RefinedHolder(n);
+// }
+
+// /*
+//  * Args domain: h.n has to be a positive number, less than the range size.
+//  * The performed refinement is only on the h.n site,
+//  * and relies on the original value replacement with the new value h.v[0];
+//  */
+
+// template <typename ForwardRange>
+// using RefinedRangeType = ::boost::joined_range<
+//     const typename ::boost::iterator_range<typename ::boost::range_iterator<const ForwardRange>::type>,
+//     const typename ::boost::iterator_range<typename ::boost::range_iterator<const ForwardRange>::type>,
+//     const typename ::boost::iterator_range<typename ::boost::range_iterator<const ForwardRange>::type>>;
+
+// template <typename ForwardRange>
+// RefinedRangeType<ForwardRange>
+// operator|(const ForwardRange &rng, const RefinedHolder &h) {
+// #ifndef NDEBUG
+//     const auto d = std::distance(std::begin(rng), std::end(rng));
+//     assert(d >= 0);
+//     assert(::boost::numeric_cast<decltype(d)>(h.n) <= d);
+//     assert(h.n >= 0);
+// #endif
+//     const auto mid = std::next(std::begin(rng), h.n);
+//     return ::boost::join(::boost::make_iterator_range(mid, std::end(rng)),
+//                          ::boost::make_iterator_range(std::begin(rng), mid));
+// }
+
+// }  // namespace extension_implementation::boost::adaptors
 
 // ########################################################
 // ##  export API to extension::boost::adaptors          ##
--- extensions_tests/src/adaptor_tests.cpp
+++ extensions_tests/src/adaptor_tests.cpp
@@ -9,54 +9,54 @@
 #include <gtest/gtest.h>
 
 TEST(ExtensionBoostAdaptorsRotated, FilledRawArray) {
-  int v1[6] = {11, 12, 13, 14, 15, 16};
-  int v2[6] = {13, 14, 15, 16, 11, 12};
-  ASSERT_EQ((v1 | extension::boost::adaptors::rotated(0)).size(), 6);
-  ASSERT_EQ((v1 | extension::boost::adaptors::rotated(2)).size(), 6);
-  ASSERT_EQ((v1 | extension::boost::adaptors::rotated(6)).size(), 6);
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(0), v1));
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(2), v2));
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(6), v1));
+    int v1[6] = {11, 12, 13, 14, 15, 16};
+    int v2[6] = {13, 14, 15, 16, 11, 12};
+    ASSERT_EQ((v1 | extension::boost::adaptors::rotated(0)).size(), 6);
+    ASSERT_EQ((v1 | extension::boost::adaptors::rotated(2)).size(), 6);
+    ASSERT_EQ((v1 | extension::boost::adaptors::rotated(6)).size(), 6);
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(0), v1));
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(2), v2));
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(6), v1));
 }
 
 TEST(ExtensionBoostAdaptorsRotated, FilledStdArray) {
-  std::array<int, 6> v1{11, 12, 13, 14, 15, 16};
-  std::array<int, 6> v2{13, 14, 15, 16, 11, 12};
-  ASSERT_EQ((v1 | extension::boost::adaptors::rotated(0)).size(), 6);
-  ASSERT_EQ((v1 | extension::boost::adaptors::rotated(2)).size(), 6);
-  ASSERT_EQ((v1 | extension::boost::adaptors::rotated(6)).size(), 6);
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(0), v1));
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(2), v2));
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(6), v1));
+    std::array<int, 6> v1{11, 12, 13, 14, 15, 16};
+    std::array<int, 6> v2{13, 14, 15, 16, 11, 12};
+    ASSERT_EQ((v1 | extension::boost::adaptors::rotated(0)).size(), 6);
+    ASSERT_EQ((v1 | extension::boost::adaptors::rotated(2)).size(), 6);
+    ASSERT_EQ((v1 | extension::boost::adaptors::rotated(6)).size(), 6);
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(0), v1));
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(2), v2));
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(6), v1));
 }
 
 TEST(ExtensionBoostAdaptorsRotated, FilledStdVector) {
-  std::vector<int> v1{11, 12, 13, 14, 15, 16};
-  std::vector<int> v2{13, 14, 15, 16, 11, 12};
-  ASSERT_EQ((v1 | extension::boost::adaptors::rotated(0)).size(), 6);
-  ASSERT_EQ((v1 | extension::boost::adaptors::rotated(2)).size(), 6);
-  ASSERT_EQ((v1 | extension::boost::adaptors::rotated(6)).size(), 6);
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(0), v1));
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(2), v2));
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(6), v1));
+    std::vector<int> v1{11, 12, 13, 14, 15, 16};
+    std::vector<int> v2{13, 14, 15, 16, 11, 12};
+    ASSERT_EQ((v1 | extension::boost::adaptors::rotated(0)).size(), 6);
+    ASSERT_EQ((v1 | extension::boost::adaptors::rotated(2)).size(), 6);
+    ASSERT_EQ((v1 | extension::boost::adaptors::rotated(6)).size(), 6);
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(0), v1));
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(2), v2));
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(6), v1));
 }
 
 TEST(ExtensionBoostAdaptorsRotated, FilledStdList) {
-  std::list<int> v1{11, 12, 13, 14, 15, 16};
-  std::list<int> v2{13, 14, 15, 16, 11, 12};
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(0), v1));
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(2), v2));
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(6), v1));
+    std::list<int> v1{11, 12, 13, 14, 15, 16};
+    std::list<int> v2{13, 14, 15, 16, 11, 12};
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(0), v1));
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(2), v2));
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(6), v1));
 }
 
 TEST(ExtensionBoostAdaptorsRotated, EmptyStdList) {
-  std::list<int> v1;
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(0), v1));
+    std::list<int> v1;
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::rotated(0), v1));
 }
 
 TEST(ExtensionBoostAdaptorsDoubled, FilledRawArray) {
-  int v1[6] = {11, 12, 13, 14, 15, 16};
-  int v2[12] = {11, 12, 13, 14, 15, 16, 11, 12, 13, 14, 15, 16};
-  ASSERT_EQ((v1 | extension::boost::adaptors::doubled).size(), 12);
-  ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::doubled, v2));
+    int v1[6] = {11, 12, 13, 14, 15, 16};
+    int v2[12] = {11, 12, 13, 14, 15, 16, 11, 12, 13, 14, 15, 16};
+    ASSERT_EQ((v1 | extension::boost::adaptors::doubled).size(), 12);
+    ASSERT_TRUE(boost::equal(v1 | extension::boost::adaptors::doubled, v2));
 }
