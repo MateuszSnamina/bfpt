8c5cb379246bd5d24179cbeb37ad716777effd14
Mateusz <mateusz.snamina@gmail.com>
2019-12-19 23:38:23 +0100

    cleaning

--- extensions_tests/src/range_streamer_test.cpp
+++ extensions_tests/src/range_streamer_test.cpp
@@ -22,62 +22,56 @@ TEST(RangeStreamer, RawRangeStreamerEmptyRange) {
 
 TEST(RangeStreamer, Fancy) {
     double v1[3] = {1.1, 1.2, 1.3};
-    const auto s = extension::boost::RangeStringStreamer()
-                       .set_stream_preparer([](std::ostream& s) { s << "BEGIN***|" << std::fixed << std::setprecision(2) << std::showpos; })
-                       .set_stream_sustainer([](std::ostream& s, size_t i) { s << std::setw(2) << char('A' + i) << ":" << std::setw(6); })
-                       .set_stream_separer([](std::ostream& s) { s << "|"; })
-                       .set_stream_finisher([](std::ostream& s) { s << "|***END"; })
-                       .stream(v1)
-                       .str();
-    ASSERT_EQ(s, "BEGIN***| A: +1.10| B: +1.20| C: +1.30|***END");
+    const auto str = extension::boost::RangeStringStreamer()
+                         .set_stream_preparer([](std::ostream& s) { s << "BEGIN***|" << std::fixed << std::setprecision(2) << std::showpos; })
+                         .set_stream_sustainer([](std::ostream& s, size_t i) { s << std::setw(2) << char('A' + i) << ":" << std::setw(6); })
+                         .set_stream_separer([](std::ostream& s) { s << "|"; })
+                         .set_stream_finisher([](std::ostream& s) { s << "|***END"; })
+                         .stream(v1)
+                         .str();
+    ASSERT_EQ(str, "BEGIN***| A: +1.10| B: +1.20| C: +1.30|***END");
 }
 
 TEST(RangeStreamer, FormatIndependencyFlagOff) {
     double v1[3] = {1.1, 1.2, 1.3};
-    std::ostringstream oss1;
-    oss1.fill('*');
-    oss1 << std::setw(5);
-    extension::boost::RangeStreamer(oss1)
+    std::ostringstream oss;
+    oss.fill('*');
+    oss << std::setw(5);
+    extension::boost::RangeStreamer(oss)
         .set_stream_preparer([](std::ostream& s) { s << "{" << std::fixed << std::setprecision(2) << std::showpos; })
-        .set_stream_sustainer([](std::ostream& s, size_t i) {
-            s << std::setw(2) << char('A' + i) << ":" << std::setw(6);
-        })
+        .set_stream_sustainer([](std::ostream& s, size_t i) { s << std::setw(2) << char('A' + i) << ":" << std::setw(6); })
         .set_stream_separer([](std::ostream& s) { s << "|"; })
         .set_format_independence_flag(false)
         .stream(v1);
-    oss1 << 1.4;
-    ASSERT_EQ(oss1.str(), "****{*A:*+1.10|*B:*+1.20|*C:*+1.30}+1.40");
+    oss << 1.4;
+    ASSERT_EQ(oss.str(), "****{*A:*+1.10|*B:*+1.20|*C:*+1.30}+1.40");
 }
 
 TEST(RangeStreamer, FormatIndependencyFlagExplicitOn) {
     double v1[3] = {1.1, 1.2, 1.3};
-    std::ostringstream oss1;
-    oss1.fill('*');
-    oss1 << std::setw(5);
-    extension::boost::RangeStreamer(oss1)
+    std::ostringstream oss;
+    oss.fill('*');
+    oss << std::setw(5);
+    extension::boost::RangeStreamer(oss)
         .set_stream_preparer([](std::ostream& s) { s << "{" << std::fixed << std::setprecision(2) << std::showpos; })
-        .set_stream_sustainer([](std::ostream& s, size_t i) {
-            s << std::setw(2) << char('A' + i) << ":" << std::setw(6);
-        })
+        .set_stream_sustainer([](std::ostream& s, size_t i) { s << std::setw(2) << char('A' + i) << ":" << std::setw(6); })
         .set_stream_separer([](std::ostream& s) { s << "|"; })
         .set_format_independence_flag(true)
         .stream(v1);
-    oss1 << 1.4;
-    ASSERT_EQ(oss1.str(), "{ A: +1.10| B: +1.20| C: +1.30}**1.4");
+    oss << 1.4;
+    ASSERT_EQ(oss.str(), "{ A: +1.10| B: +1.20| C: +1.30}**1.4");
 }
 
 TEST(RangeStreamer, FormatIndependencyFlagImplicitOn) {
     double v1[3] = {1.1, 1.2, 1.3};
-    std::ostringstream oss1;
-    oss1.fill('*');
-    oss1 << std::setw(5);
-    extension::boost::RangeStreamer(oss1)
+    std::ostringstream oss;
+    oss.fill('*');
+    oss << std::setw(5);
+    extension::boost::RangeStreamer(oss)
         .set_stream_preparer([](std::ostream& s) { s << "{" << std::fixed << std::setprecision(2) << std::showpos; })
-        .set_stream_sustainer([](std::ostream& s, size_t i) {
-            s << std::setw(2) << char('A' + i) << ":" << std::setw(6);
-        })
+        .set_stream_sustainer([](std::ostream& s, size_t i) { s << std::setw(2) << char('A' + i) << ":" << std::setw(6); })
         .set_stream_separer([](std::ostream& s) { s << "|"; })
         .stream(v1);
-    oss1 << 1.4;
-    ASSERT_EQ(oss1.str(), "{ A: +1.10| B: +1.20| C: +1.30}**1.4");
+    oss << 1.4;
+    ASSERT_EQ(oss.str(), "{ A: +1.10| B: +1.20| C: +1.30}**1.4");
 }
