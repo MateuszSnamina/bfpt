8fee3a6eeb6579c5e3f20a62d4386aaf0f415926
Mateusz <mateusz.snamina@gmail.com>
2020-01-04 18:01:52 +0100

    k neq 0 works, but uses cx_mat

--- model_monostar/src/main.cpp
+++ model_monostar/src/main.cpp
@@ -1,3 +1,4 @@
+#include <model_monostar/hardcoded_example.hpp>
 #include <model_monostar/monostar_basis.hpp>
 #include <model_monostar/monostar_hamiltonian_k0.hpp>
 #include <model_monostar/monostar_kstate.hpp>
@@ -11,6 +12,9 @@
 #include <iterator>
 
 #include <cassert>  // TODO:  move to hamiltonian sourcefile
+#include <complex>
+
+using namespace std::complex_literals;
 
 // #######################################################################
 // ## DynamicMonostarHamiltonian                                        ##
@@ -38,13 +42,13 @@ class DynamicMonostarHamiltonian {
     void fill_kn_hamiltonian_matrix_coll(
         const DynamicMonostarUniqueKstateBasis& basis,
         size_t n_col,
-        arma::mat& kn_hamiltonian_matrix,
+        arma::cx_mat& kn_hamiltonian_matrix,
         const unsigned k_n) const;
     void fill_kn_hamiltonian_matrix(
         const DynamicMonostarUniqueKstateBasis& basis,
-        arma::mat& kn_hamiltonian_matrix,
+        arma::cx_mat& kn_hamiltonian_matrix,
         const unsigned k_n) const;
-    arma::mat make_kn_hamiltonian_matrix(
+    arma::cx_mat make_kn_hamiltonian_matrix(
         const DynamicMonostarUniqueKstateBasis& basis,
         const unsigned k_n) const;
 
@@ -78,7 +82,7 @@ inline void DynamicMonostarHamiltonian::push_back_conjugated_states_to_basis(
     }
 }
 
-/// ------------------------------ arbitrary k_n = 0:
+/// ------------------------------ for k_n = 0:
 
 inline void DynamicMonostarHamiltonian::fill_k0_hamiltonian_matrix_coll(
     const DynamicMonostarUniqueKstateBasis& basis,
@@ -141,7 +145,7 @@ DynamicMonostarHamiltonian::make_k0_hamiltonian_matrix(
 inline void DynamicMonostarHamiltonian::fill_kn_hamiltonian_matrix_coll(
     const DynamicMonostarUniqueKstateBasis& basis,
     const size_t ket_kstate_idx,
-    arma::mat& kn_hamiltonian_matrix,
+    arma::cx_mat& kn_hamiltonian_matrix,
     const unsigned k_n) const {
     assert(kn_hamiltonian_matrix.n_cols == basis.size());
     assert(kn_hamiltonian_matrix.n_rows == basis.size());
@@ -154,8 +158,17 @@ inline void DynamicMonostarHamiltonian::fill_kn_hamiltonian_matrix_coll(
             const auto bra_kstate = ket_kstate | extension::boost::adaptors::refined(n_delta, es) | extension::boost::adaptors::refined(n_delta_p1, es);
             if (const auto& bra_kstate_optional_idx = basis.find_element_and_get_its_ra_index(kstate::make_unique_shift(bra_kstate))) {
                 const auto bra_kstate_idx = *bra_kstate_optional_idx;
-                double pre_norm = _n_sites * (_n_sites / basis.vec_index()[bra_kstate_idx]->n_least_replication_shift()) * basis.vec_index()[bra_kstate_idx]->norm_factor() * basis.vec_index()[ket_kstate_idx]->norm_factor();
-                kn_hamiltonian_matrix(bra_kstate_idx, ket_kstate_idx) += pre_norm * 0.5;
+                double pre_norm = _n_sites * basis.vec_index()[bra_kstate_idx]->norm_factor() * basis.vec_index()[ket_kstate_idx]->norm_factor();
+                std::complex<double> sum_phase_factors = 0;
+                const size_t bra_n_unique_shift = kstate::n_unique_shift(bra_kstate);
+                const size_t bra_n_least_replication_shift = basis.vec_index()[bra_kstate_idx]->n_least_replication_shift();
+                const size_t bra_n_replicas = _n_sites / bra_n_least_replication_shift;
+                for (unsigned n_contribution = 0; n_contribution < bra_n_replicas; n_contribution++) {
+                    // sign in front of bra_n_unique_shift in the below formula has to be check!
+                    const double exponent = 2 * arma::datum::pi * k_n / _n_sites * (-(int)bra_n_unique_shift + (int)n_contribution * (int)bra_n_least_replication_shift);
+                    sum_phase_factors += std::exp(1.0i * exponent);
+                }
+                kn_hamiltonian_matrix(bra_kstate_idx, ket_kstate_idx) += pre_norm * sum_phase_factors * 0.5;
             }
         }
     }
@@ -165,8 +178,22 @@ inline void DynamicMonostarHamiltonian::fill_kn_hamiltonian_matrix_coll(
             const auto bra_kstate = ket_kstate | extension::boost::adaptors::refined(n_delta, gs) | extension::boost::adaptors::refined(n_delta_p1, gs);
             if (const auto& bra_kstate_optional_idx = basis.find_element_and_get_its_ra_index(kstate::make_unique_shift(bra_kstate))) {
                 const auto bra_kstate_idx = *bra_kstate_optional_idx;
-                double pre_norm = _n_sites * (_n_sites / basis.vec_index()[bra_kstate_idx]->n_least_replication_shift()) * basis.vec_index()[bra_kstate_idx]->norm_factor() * basis.vec_index()[ket_kstate_idx]->norm_factor();
-                kn_hamiltonian_matrix(bra_kstate_idx, ket_kstate_idx) += pre_norm * 0.5;
+                double pre_norm = _n_sites * basis.vec_index()[bra_kstate_idx]->norm_factor() * basis.vec_index()[ket_kstate_idx]->norm_factor();
+                std::complex<double> sum_phase_factors = 0;
+                const size_t bra_n_unique_shift = kstate::n_unique_shift(bra_kstate);
+                const size_t bra_n_least_replication_shift = basis.vec_index()[bra_kstate_idx]->n_least_replication_shift();
+                const size_t bra_n_replicas = _n_sites / bra_n_least_replication_shift;
+                for (unsigned n_contribution = 0; n_contribution < bra_n_replicas; n_contribution++) {
+                    const double exponent = 2 * arma::datum::pi * k_n / _n_sites * (-(int)bra_n_unique_shift + (int)n_contribution * (int)bra_n_least_replication_shift);
+                    sum_phase_factors += std::exp(1.0i * exponent);
+                    std::cout << "IDXs " << bra_kstate_idx << " x " << ket_kstate_idx << std::endl;
+                    std::cout << bra_kstate << " x " << ket_kstate << std::endl;
+                    std::cout << "n_contribution " << n_contribution << ", "
+                              << "bra_n_unique_shift " << bra_n_unique_shift << ", "
+                              << "exponent/pi " << 2.0 * k_n / _n_sites * (-(int)bra_n_unique_shift + (int)n_contribution * (int)bra_n_least_replication_shift) << ", "
+                              << std::endl;
+                }
+                kn_hamiltonian_matrix(bra_kstate_idx, ket_kstate_idx) += pre_norm * sum_phase_factors * 0.5;
             }
         }
     }
@@ -179,7 +206,7 @@ inline void DynamicMonostarHamiltonian::fill_kn_hamiltonian_matrix_coll(
 
 inline void DynamicMonostarHamiltonian::fill_kn_hamiltonian_matrix(
     const DynamicMonostarUniqueKstateBasis& basis,
-    arma::mat& kn_hamiltonian_matrix,
+    arma::cx_mat& kn_hamiltonian_matrix,
     const unsigned k_n) const {
     assert(kn_hamiltonian_matrix.n_cols == basis.size());
     assert(kn_hamiltonian_matrix.n_rows == basis.size());
@@ -188,11 +215,11 @@ inline void DynamicMonostarHamiltonian::fill_kn_hamiltonian_matrix(
     }
 }
 
-inline arma::mat
+inline arma::cx_mat
 DynamicMonostarHamiltonian::make_kn_hamiltonian_matrix(
     const DynamicMonostarUniqueKstateBasis& basis,
     const unsigned k_n) const {
-    arma::mat kn_hamiltonian_matrix(basis.size(), basis.size(), arma::fill::zeros);
+    arma::cx_mat kn_hamiltonian_matrix(basis.size(), basis.size(), arma::fill::zeros);
     fill_kn_hamiltonian_matrix(basis, kn_hamiltonian_matrix, k_n);
     return kn_hamiltonian_matrix;
 }
@@ -250,7 +277,35 @@ double bfpt_gs(const size_t n_sites, const unsigned max_pt_order) {
     return eigen_values(0);
 }
 
-double bfpt_k0_es(const size_t n_sites, const unsigned max_pt_order, const unsigned k_n) {
+double bfpt_goldston(const size_t n_sites, const unsigned max_pt_order) {
+    // Define hamiltonian and basis:
+    model_monostar::DynamicMonostarUniqueKstateBasis basis{n_sites};
+    model_monostar::DynamicMonostarHamiltonian hamiltonian{n_sites};
+    // Define pt_orders subspace basis:
+    basis.add_element(std::make_shared<model_monostar::DynamicMonostarUniqueKstate>(model_monostar::classical_es_kstate(n_sites)));
+    // ----
+    std::cout << basis;
+    // ----
+    // Generate higher pt_orders subspace basis:
+    populate_pt_basis(hamiltonian, max_pt_order, basis);
+    // ----
+    std::cout << basis;
+    // ----
+    const auto k0_hamiltonian_matrix = hamiltonian.make_k0_hamiltonian_matrix(basis);
+    // ----
+    // std::cout << k0_hamiltonian_matrix;
+    // ----
+    arma::vec eigen_values;
+    arma::mat eigen_vectors;
+    arma::eig_sym(eigen_values, eigen_vectors, k0_hamiltonian_matrix);
+    // ----
+    std::cout << eigen_values;
+    std::cout << "min eigen_value: " << eigen_values(0) << std::endl;
+    // ----
+    return eigen_values(0);
+}
+
+double bfpt_kn_es(const size_t n_sites, const unsigned max_pt_order, const unsigned k_n) {
     // Define hamiltonian and basis:
     model_monostar::DynamicMonostarUniqueKstateBasis basis{n_sites};
     model_monostar::DynamicMonostarHamiltonian hamiltonian{n_sites};
@@ -266,10 +321,10 @@ double bfpt_k0_es(const size_t n_sites, const unsigned max_pt_order, const unsig
     // ----
     const auto kn_hamiltonian_matrix = hamiltonian.make_kn_hamiltonian_matrix(basis, k_n);
     // ----
-    // std::cout << kn_hamiltonian_matrix;
+    std::cout << kn_hamiltonian_matrix;
     // ----
     arma::vec eigen_values;
-    arma::mat eigen_vectors;
+    arma::cx_mat eigen_vectors;
     arma::eig_sym(eigen_values, eigen_vectors, kn_hamiltonian_matrix);
     // ----
     std::cout << eigen_values;
@@ -279,14 +334,24 @@ double bfpt_k0_es(const size_t n_sites, const unsigned max_pt_order, const unsig
 }
 
 int main() {
-    const unsigned max_pt_order = 3;
-    const size_t n_sites = 20;
-    const unsigned k_n = 0;
+    const unsigned max_pt_order = 1;
+    const size_t n_sites = 8;
+    const unsigned k_n = 1;
+    std::cout << "------------------------------------------" << std::endl;
     double gs_energy = bfpt_gs(n_sites, max_pt_order);
-    double es_energy = bfpt_k0_es(n_sites, max_pt_order, 0);
-    std::cout << " k_n = " << k_n << std::endl;
+    std::cout << "------------------------------------------" << std::endl;
+    double es_gold_energy = bfpt_goldston(n_sites, max_pt_order);
+    std::cout << "------------------------------------------" << std::endl;
+    double es_energy = bfpt_kn_es(n_sites, max_pt_order, k_n);
+    std::cout << "------------------------------------------" << std::endl;
     std::cout << " gs enery = " << gs_energy << std::endl;
+    std::cout << " es goldston = " << es_gold_energy << std::endl;
+    std::cout << "    excitation enery goldston = " << es_gold_energy - gs_energy << std::endl;
+    std::cout << " k_n = " << k_n << std::endl;
     std::cout << " es enery (k_n) = " << es_energy << std::endl;
-    std::cout << " excitation enery (k_n) = " << es_energy - gs_energy << std::endl;
+    std::cout << "    excitation enery (k_n) = " << es_energy - gs_energy << std::endl;
+    std::cout << "------------------------------------------" << std::endl;
+    std::cout << "------------------------------------------" << std::endl;
+    //model_monostar::do_hardcoded_example_analyse();
     return 0;
 }
