16065a8878c3c7affbdc5e36748da7cb21a9b544
Mateusz <mateusz.snamina@gmail.com>
2020-01-05 22:30:58 +0100

    remove kn=0 case

--- model_monostar/src/main.cpp
+++ model_monostar/src/main.cpp
@@ -26,20 +26,11 @@ namespace model_monostar {
 class DynamicMonostarHamiltonian {
    public:
     DynamicMonostarHamiltonian(const size_t n_sites);
-    void push_back_conjugated_states_to_basis(
+    // Generates all conjugated states:
+    void push_back_coupled_states_to_basis(
         const DynamicMonostarUniqueKstate& generator,
         DynamicMonostarUniqueKstateBasis& basis) const;
-    // ---- for: k_n =0
-    void fill_k0_hamiltonian_matrix_coll(
-        const DynamicMonostarUniqueKstateBasis& basis,
-        size_t n_col,
-        arma::mat& k0_hamiltonian_matrix) const;
-    void fill_k0_hamiltonian_matrix(
-        const DynamicMonostarUniqueKstateBasis& basis,
-        arma::mat& k0_hamiltonian_matrix) const;
-    arma::mat make_k0_hamiltonian_matrix(
-        const DynamicMonostarUniqueKstateBasis& basis) const;
-    // ---- for arbitrary k_n
+    // Generates hamiltonian matrix:
     void fill_kn_hamiltonian_matrix_coll(
         const DynamicMonostarUniqueKstateBasis& basis,
         size_t n_col,
@@ -60,7 +51,7 @@ class DynamicMonostarHamiltonian {
 inline DynamicMonostarHamiltonian::DynamicMonostarHamiltonian(const size_t n_sites)
     : _n_sites(n_sites) {}
 
-inline void DynamicMonostarHamiltonian::push_back_conjugated_states_to_basis(
+inline void DynamicMonostarHamiltonian::push_back_coupled_states_to_basis(
     const DynamicMonostarUniqueKstate& generator,
     DynamicMonostarUniqueKstateBasis& basis) const {
     assert(generator.n_sites() == _n_sites);
@@ -81,64 +72,6 @@ inline void DynamicMonostarHamiltonian::push_back_conjugated_states_to_basis(
             basis.add_element(conjugated_kstate_ptr);
         }
     }
-}
-
-/// ------------------------------ for k_n = 0:
-
-inline void DynamicMonostarHamiltonian::fill_k0_hamiltonian_matrix_coll(
-    const DynamicMonostarUniqueKstateBasis& basis,
-    const size_t ket_kstate_idx,
-    arma::mat& k0_hamiltonian_matrix) const {
-    assert(k0_hamiltonian_matrix.n_cols == basis.size());
-    assert(k0_hamiltonian_matrix.n_rows == basis.size());
-    const auto ket_kstate_ptr = basis.vec_index()[ket_kstate_idx];
-    assert(ket_kstate_ptr);
-    const auto& ket_kstate = (*ket_kstate_ptr).to_range();
-    for (size_t n_delta = 0, n_delta_p1 = 1; n_delta < _n_sites; n_delta++, n_delta_p1 = (n_delta + 1) % _n_sites) {
-        if (*std::next(std::begin(ket_kstate), n_delta) == gs &&
-            *std::next(std::begin(ket_kstate), n_delta_p1) == gs) {
-            const auto bra_kstate = ket_kstate | extension::boost::adaptors::refined(n_delta, es) | extension::boost::adaptors::refined(n_delta_p1, es);
-            if (const auto& bra_kstate_optional_idx = basis.find_element_and_get_its_ra_index(kstate::make_unique_shift(bra_kstate))) {
-                const auto bra_kstate_idx = *bra_kstate_optional_idx;
-                double pre_norm = _n_sites * (_n_sites / basis.vec_index()[bra_kstate_idx]->n_least_replication_shift()) * basis.vec_index()[bra_kstate_idx]->norm_factor() * basis.vec_index()[ket_kstate_idx]->norm_factor();
-                k0_hamiltonian_matrix(bra_kstate_idx, ket_kstate_idx) = k0_hamiltonian_matrix(bra_kstate_idx, ket_kstate_idx) + pre_norm * 0.5;
-            }
-        }
-    }
-    for (size_t n_delta = 0, n_delta_p1 = 1; n_delta < _n_sites; n_delta++, n_delta_p1 = (n_delta + 1) % _n_sites) {
-        if (*std::next(std::begin(ket_kstate), n_delta) == es &&
-            *std::next(std::begin(ket_kstate), n_delta_p1) == es) {
-            const auto bra_kstate = ket_kstate | extension::boost::adaptors::refined(n_delta, gs) | extension::boost::adaptors::refined(n_delta_p1, gs);
-            if (const auto& bra_kstate_optional_idx = basis.find_element_and_get_its_ra_index(kstate::make_unique_shift(bra_kstate))) {
-                const auto bra_kstate_idx = *bra_kstate_optional_idx;
-                double pre_norm = _n_sites * (_n_sites / basis.vec_index()[bra_kstate_idx]->n_least_replication_shift()) * basis.vec_index()[bra_kstate_idx]->norm_factor() * basis.vec_index()[ket_kstate_idx]->norm_factor();
-                k0_hamiltonian_matrix(bra_kstate_idx, ket_kstate_idx) = k0_hamiltonian_matrix(bra_kstate_idx, ket_kstate_idx) + pre_norm * 0.5;
-            }
-        }
-    }
-    for (size_t n_delta = 0, n_delta_p1 = 1; n_delta < _n_sites; n_delta++, n_delta_p1 = (n_delta + 1) % _n_sites) {
-        const bool is_the_same = *std::next(std::begin(ket_kstate), n_delta) == *std::next(std::begin(ket_kstate), n_delta_p1);
-        double pre_norm = _n_sites * (_n_sites / basis.vec_index()[ket_kstate_idx]->n_least_replication_shift()) * basis.vec_index()[ket_kstate_idx]->norm_factor() * basis.vec_index()[ket_kstate_idx]->norm_factor();
-        k0_hamiltonian_matrix(ket_kstate_idx, ket_kstate_idx) += pre_norm * (is_the_same ? -1.0 / 4.0 : +1.0 / 4.0);
-    }
-}
-
-inline void DynamicMonostarHamiltonian::fill_k0_hamiltonian_matrix(
-    const DynamicMonostarUniqueKstateBasis& basis,
-    arma::mat& k0_hamiltonian_matrix) const {
-    assert(k0_hamiltonian_matrix.n_cols == basis.size());
-    assert(k0_hamiltonian_matrix.n_rows == basis.size());
-    for (arma::uword ket_kstate_idx = 0; ket_kstate_idx < basis.size(); ket_kstate_idx++) {
-        fill_k0_hamiltonian_matrix_coll(basis, ket_kstate_idx, k0_hamiltonian_matrix);
-    }
-}
-
-inline arma::mat
-DynamicMonostarHamiltonian::make_k0_hamiltonian_matrix(
-    const DynamicMonostarUniqueKstateBasis& basis) const {
-    arma::mat k0_hamiltonian_matrix(basis.size(), basis.size(), arma::fill::zeros);
-    fill_k0_hamiltonian_matrix(basis, k0_hamiltonian_matrix);
-    return k0_hamiltonian_matrix;
 }
 
 /// ------------------------------ arbitrary k_n:
@@ -235,7 +168,6 @@ void populate_pt_basis(
     const model_monostar::DynamicMonostarHamiltonian& hamiltonian,
     const unsigned max_pt_order,
     model_monostar::DynamicMonostarUniqueKstateBasis& basis) {
-    // assert();
     unsigned last_chunk_size = basis.size();
     for (unsigned pt_order = 0; pt_order < max_pt_order; pt_order++) {
         const unsigned old_size = basis.size();
@@ -243,7 +175,7 @@ void populate_pt_basis(
         for (unsigned idx = old_size - last_chunk_size; idx < old_size; idx++) {
             const auto el = basis.vec_index()[idx];
             assert(el);
-            hamiltonian.push_back_conjugated_states_to_basis(*el, basis);
+            hamiltonian.push_back_coupled_states_to_basis(*el, basis);
         }
         const unsigned new_size = basis.size();
         last_chunk_size = new_size - old_size;
@@ -264,13 +196,13 @@ double bfpt_gs(const size_t n_sites, const unsigned max_pt_order) {
     // ----
     std::cout << basis;
     // ----
-    const auto k0_hamiltonian_matrix = hamiltonian.make_k0_hamiltonian_matrix(basis);
+    const auto k0_hamiltonian_matrix = hamiltonian.make_kn_hamiltonian_matrix(basis, 0);
     // ----
     // std::cout << k0_hamiltonian_matrix;
     // ----
     arma::vec eigen_values;
-    arma::mat eigen_vectors;
-    arma::eig_sym(eigen_values, eigen_vectors, k0_hamiltonian_matrix);
+    arma::cx_mat eigen_vectors;
+    lin_alg::eigs_sym(eigen_values, eigen_vectors, k0_hamiltonian_matrix, 1, "sa", 1e-6);
     // ----
     std::cout << eigen_values;
     std::cout << "min eigen_value: " << eigen_values(0) << std::endl;
@@ -292,13 +224,13 @@ double bfpt_goldston(const size_t n_sites, const unsigned max_pt_order) {
     // ----
     std::cout << basis;
     // ----
-    const auto k0_hamiltonian_matrix = hamiltonian.make_k0_hamiltonian_matrix(basis);
+    const auto k0_hamiltonian_matrix = hamiltonian.make_kn_hamiltonian_matrix(basis, 0);
     // ----
     // std::cout << k0_hamiltonian_matrix;
     // ----
     arma::vec eigen_values;
-    arma::mat eigen_vectors;
-    arma::eig_sym(eigen_values, eigen_vectors, k0_hamiltonian_matrix);
+    arma::cx_mat eigen_vectors;
+    lin_alg::eigs_sym(eigen_values, eigen_vectors, k0_hamiltonian_matrix, 1, "sa", 1e-6);
     // ----
     std::cout << eigen_values;
     std::cout << "min eigen_value: " << eigen_values(0) << std::endl;
