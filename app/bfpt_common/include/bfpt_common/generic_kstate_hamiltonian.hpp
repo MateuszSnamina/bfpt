71acede6dc206dfc1e4a5693f5e79a474ac0e55a
snaminm <mateusz.snamina@gmail.com>
2020-09-27 16:22:24 +0200

    add kernel1 : devel midpoint 2

--- app/bfpt_common/include/bfpt_common/generic_kstate_hamiltonian.hpp
+++ app/bfpt_common/include/bfpt_common/generic_kstate_hamiltonian.hpp
@@ -141,13 +141,39 @@ GenericKstateHamiltonian<_SiteStateT>::fill_kn_hamiltonian_matrix_coll(
     const auto ket_kstate_ptr = basis.vec_index()[ket_kstate_idx];
     assert(ket_kstate_ptr);
     const auto& ket_kstate = (*ket_kstate_ptr).to_range();
-
-//    for (size_t n_delta = 0; n_delta < _n_sites; n_delta++) {
-//TODO!!!!
-//        } // end of `_half_off_diag_info` equal_range loop
-//    } // end of filling hamiltonian1-off0siag loop
-
-
+    // ********** OFF-DIAG, HAMILTONIAN1 *********************************************
+    for (size_t n_delta = 0; n_delta < _n_sites; n_delta++) {
+        const auto ket_kernel_site_1 = *std::next(std::begin(ket_kstate), n_delta);
+        const StateKernel1<SiteStateT> ket_kernel{ket_kernel_site_1};
+        const auto equal_range = _hamiltonian_kernel_1._half_off_diag_info.equal_range(ket_kernel);
+        for (auto off_diag_node_it = equal_range.first; off_diag_node_it != equal_range.second; ++off_diag_node_it) {
+            const auto& ket_kernel_re = off_diag_node_it->first;
+            [[maybe_unused]] const auto& ket_kernel_site_1_re = ket_kernel_re.state_1;
+            assert(ket_kernel_site_1_re == ket_kernel_site_1);
+            const auto& couple_info = off_diag_node_it->second;
+            const auto& kernel_coupling_coef = couple_info.coef;
+            const auto& bra_kernel = couple_info.kernel_state;
+            const auto& bra_kernel_site_1 = bra_kernel.state_1;
+            const auto bra_kstate = ket_kstate
+                    | extension::boost::adaptors::refined(n_delta, bra_kernel_site_1);
+            const size_t bra_n_unique_shift = kstate::n_unique_shift(bra_kstate);
+            const auto bra_kstate_unique_shifted = bra_kstate | extension::boost::adaptors::rotated(bra_n_unique_shift); // equivalent to `kstate::make_unique_shift(bra_kstate)`
+            if (const auto& bra_kstate_optional_idx = basis.find_element_and_get_its_ra_index(bra_kstate_unique_shifted)) {
+                const auto bra_kstate_idx = *bra_kstate_optional_idx;
+                double pre_norm_1 = _n_sites * basis.vec_index()[bra_kstate_idx]->norm_factor() * basis.vec_index()[ket_kstate_idx]->norm_factor();
+                const size_t bra_n_unique_shift = kstate::n_unique_shift(bra_kstate);
+                const size_t bra_n_least_replication_shift = basis.vec_index()[bra_kstate_idx]->n_least_replication_shift();
+                const size_t bra_n_replicas = _n_sites / bra_n_least_replication_shift;
+                const int exponent_n = (int)bra_n_unique_shift;
+                const double exponent_r = 2 * arma::datum::pi * k_n / _n_sites * exponent_n;
+                assert(k_n * bra_n_least_replication_shift % _n_sites == 0);
+                const std::complex<double> neo_sum_phase_factors = std::exp(1.0i * exponent_r) * (double)bra_n_replicas;
+                const std::complex<double> pre_norm_2 = pre_norm_1 * neo_sum_phase_factors;
+                kn_hamiltonian_matrix(bra_kstate_idx, ket_kstate_idx) += pre_norm_2 * kernel_coupling_coef;
+            }
+        } // end of `_half_off_diag_info` equal_range loop
+    } // end of `Delta` loop
+    // ********** OFF-DIAG, HAMILTONIAN12 ********************************************
     //std::chrono::high_resolution_clock::time_point tp_u_1, tp_u_2; // performance debug sake
     //std::chrono::high_resolution_clock::time_point tp_nu_1, tp_nu_2; // performance debug sake
     //double unique_shift_time = 0.0, not_unique_shift_time = 0.0; // performance debug sake
@@ -175,14 +201,14 @@ GenericKstateHamiltonian<_SiteStateT>::fill_kn_hamiltonian_matrix_coll(
             //tp_nu_2 = std::chrono::high_resolution_clock::now(); // performance debug sake
             //not_unique_shift_time += std::chrono::duration_cast<std::chrono::nanoseconds>(tp_nu_2 - tp_nu_1).count(); // performance debug sake
             //tp_u_1 = std::chrono::high_resolution_clock::now(); // performance debug sake
-            const auto bra_kstate_unique_shifted = kstate::make_unique_shift(bra_kstate);
+            const size_t bra_n_unique_shift = kstate::n_unique_shift(bra_kstate);
+            const auto bra_kstate_unique_shifted = bra_kstate | extension::boost::adaptors::rotated(bra_n_unique_shift); // equivalent to `kstate::make_unique_shift(bra_kstate)`
             //tp_u_2 = std::chrono::high_resolution_clock::now(); // performance debug sake
             //unique_shift_time += std::chrono::duration_cast<std::chrono::nanoseconds>(tp_u_2 - tp_u_1).count(); // performance debug sake
             //tp_nu_1 = std::chrono::high_resolution_clock::now(); // performance debug sake
             if (const auto& bra_kstate_optional_idx = basis.find_element_and_get_its_ra_index(bra_kstate_unique_shifted)) {
                 const auto bra_kstate_idx = *bra_kstate_optional_idx;
                 double pre_norm_1 = _n_sites * basis.vec_index()[bra_kstate_idx]->norm_factor() * basis.vec_index()[ket_kstate_idx]->norm_factor();
-                const size_t bra_n_unique_shift = kstate::n_unique_shift(bra_kstate);
                 const size_t bra_n_least_replication_shift = basis.vec_index()[bra_kstate_idx]->n_least_replication_shift();
                 const size_t bra_n_replicas = _n_sites / bra_n_least_replication_shift;
                 const int exponent_n = (int)bra_n_unique_shift;
@@ -193,7 +219,7 @@ GenericKstateHamiltonian<_SiteStateT>::fill_kn_hamiltonian_matrix_coll(
                 kn_hamiltonian_matrix(bra_kstate_idx, ket_kstate_idx) += pre_norm_2 * kernel_coupling_coef;
             }
         } // end of `_half_off_diag_info` equal_range loop
-    }  // end of `Delta` loop
+    } // end of `Delta` loop
     //const auto tp_offdiag_2 = std::chrono::high_resolution_clock::now(); // performance debug sake
     //tp_nu_2 = std::chrono::high_resolution_clock::now(); // performance debug sake
     //not_unique_shift_time += std::chrono::duration_cast<std::chrono::nanoseconds>(tp_nu_2 - tp_nu_1).count(); // performance debug sake
@@ -201,6 +227,9 @@ GenericKstateHamiltonian<_SiteStateT>::fill_kn_hamiltonian_matrix_coll(
     //std::cout << "[OFF-DIAG] [TIMING]: offdiag_time          : " << offdiag_time << std::endl; // performance debug sake
     //std::cout << "[OFF-DIAG] [TIMING]: unique_shift_time     : " << unique_shift_time << std::endl; // performance debug sake
     //std::cout << "[OFF-DIAG] [TIMING]: not_unique_shift_time : " << not_unique_shift_time << std::endl; // performance debug sake
+    // ********** ON-DIAG, HAMILTONIAN1 **********************************************
+    // TODO
+    // ********** ON-DIAG, HAMILTONIAN12 *********************************************
     for (size_t n_delta = 0, n_delta_p1 = 1; n_delta < _n_sites; n_delta++, n_delta_p1 = (n_delta + 1) % _n_sites) {
         const auto ket_kernel_site_1 = *std::next(std::begin(ket_kstate), n_delta);
         const auto ket_kernel_site_2 = *std::next(std::begin(ket_kstate), n_delta_p1);
