c9e6c87fefad8f72163bac6a1e76cbae256de840
snaminm <mateusz.snamina@gmail.com>
2020-10-04 11:30:04 +0200

    devel

--- app/bfpt_common/include/bfpt_common/calculate_reduced_density_operator.hpp
+++ app/bfpt_common/include/bfpt_common/calculate_reduced_density_operator.hpp
@@ -40,25 +40,34 @@ calculate_reduced_density_operator_12(
             const auto ket_kstate_range = (*ket_kstate_ptr).to_range();
             assert(boost::size(bra_kstate_range) == n_sites);
             assert(boost::size(ket_kstate_range) == n_sites);
-            const auto bra_kstate_range_rest = bra_kstate_range | boost::adaptors::sliced(2, n_sites);
-            const auto ket_kstate_range_rest = ket_kstate_range | boost::adaptors::sliced(2, n_sites);
-            if (boost::equal(bra_kstate_range_rest, ket_kstate_range_rest)) {
-                const auto bra_site_0 = *std::next(std::begin(bra_kstate_range), 0);
-                const auto bra_site_1 = *std::next(std::begin(bra_kstate_range), 1);
-                const auto ket_site_0 = *std::next(std::begin(ket_kstate_range), 0);
-                const auto ket_site_1 = *std::next(std::begin(ket_kstate_range), 1);
-                const StateKernel12 bra_kenrel{bra_site_0, bra_site_1};
-                const StateKernel12 ket_kenrel{ket_site_0, ket_site_1};
-                const std::pair<StateKernel12<KstateT>, StateKernel12<KstateT>> density_matrix_indices{bra_kenrel, ket_kenrel};
-                const std::complex<double> value = eigen_vector(bra_kstate_idx) * eigen_vector(ket_kstate_idx);
-                if (result.count(density_matrix_indices)) {
-                    result[density_matrix_indices] += value;
-                } else {
-                    result[density_matrix_indices] = value;
-                }
-            }
-        }
-    }
+            for (unsigned bra_shift = 0; bra_shift < n_sites; bra_shift++) {
+                for (unsigned ket_shift = 0; ket_shift < n_sites; ket_shift++) {
+                    const auto bra_kstate_range_rotated = bra_kstate_range | extension::boost::adaptors::rotated(bra_shift);
+                    const auto ket_kstate_range_rotated = ket_kstate_range | extension::boost::adaptors::rotated(ket_shift);
+                    const auto bra_kstate_range_rotated_rest = bra_kstate_range_rotated | boost::adaptors::sliced(2, n_sites);
+                    const auto ket_kstate_range_rotated_rest = ket_kstate_range_rotated | boost::adaptors::sliced(2, n_sites);
+                    if (boost::equal(bra_kstate_range_rotated_rest, ket_kstate_range_rotated_rest)) {
+                        const auto bra_site_0 = *std::next(std::begin(bra_kstate_range_rotated), 0);
+                        const auto bra_site_1 = *std::next(std::begin(bra_kstate_range_rotated), 1);
+                        const auto ket_site_0 = *std::next(std::begin(ket_kstate_range_rotated), 0);
+                        const auto ket_site_1 = *std::next(std::begin(ket_kstate_range_rotated), 1);
+                        const StateKernel12 bra_kenrel{bra_site_0, bra_site_1};
+                        const StateKernel12 ket_kenrel{ket_site_0, ket_site_1};
+                        const std::pair<StateKernel12<KstateT>, StateKernel12<KstateT>> density_matrix_indices{bra_kenrel, ket_kenrel};
+                        const std::complex<double> value = eigen_vector(bra_kstate_idx) * eigen_vector(ket_kstate_idx);
+                        //TODO miltiply the states norm!!!
+                        //double pre_norm = bra_kstate_ptr->norm_factor() * ket_kstate_ptr->norm_factor();
+                        if (result.count(density_matrix_indices)) {
+                            result[density_matrix_indices] += value;
+                        } else {
+                            result[density_matrix_indices] = value;
+                        }
+                    }
+                } // end of ket_shift loop
+            } // end of bra_shift loop
+        } // end of ket_kstate_idx loop
+    } // end of bra_kstate_idx loop
+    assert(false); //TODO finish implementation! => finish the norm factor part.
     return result;
 }
 
