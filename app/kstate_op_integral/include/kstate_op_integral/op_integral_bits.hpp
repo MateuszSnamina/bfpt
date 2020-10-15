e57bdc2dab679626c37e78004a42cb6591e0225f
snaminm <mateusz.snamina@gmail.com>
2020-10-15 21:21:08 +0200

    devel

--- app/kstate_op_integral/include/kstate_op_integral/op_integral_bits.hpp
+++ app/kstate_op_integral/include/kstate_op_integral/op_integral_bits.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <boost/range.hpp>
-#include <boost/range/any_range.hpp>
+#include <boost/range/irange.hpp>
+#include <boost/range/adaptor/transformed.hpp>
 
 #include <iterator>
 #include <type_traits>
@@ -39,6 +40,51 @@ T rotate(T n, unsigned n_all_bits, unsigned idx_pivot_bit) {
     const T low_bits = n & low_bits_mask;
     const T high_bits = n & high_bits_mask;
     const T result = (high_bits >> n_low_bits) | (low_bits << n_high_bits);
+    return result;
+}
+
+template <typename T>
+T refine(T n, bool new_value, unsigned idx_bit) {
+    static_assert(std::is_arithmetic_v<T>);
+    static_assert(std::is_integral_v<T>);
+    static_assert(std::is_unsigned_v<T>);
+    const T mask = (static_cast<T>(1u) << idx_bit);
+    const T result = (new_value ? n | mask : n & ~mask);
+    return result;
+}
+
+template <typename T>
+auto integral_to_bits_range(T n, unsigned n_all_bits) {
+    static_assert(std::is_arithmetic_v<T>);
+    static_assert(std::is_integral_v<T>);
+    static_assert(std::is_unsigned_v<T>);
+    assert(n_all_bits < 8 * sizeof(T));
+    const auto r = boost::irange(0u, n_all_bits) | boost::adaptors::transformed(
+                [n](unsigned idx_bit) -> bool {return extract_bit(n, idx_bit);}
+    );
+    return r;
+}
+
+template <typename T, typename RangeT>
+T integral_from_bits_range(RangeT r) {
+    static_assert(std::is_arithmetic_v<T>);
+    static_assert(std::is_integral_v<T>);
+    static_assert(std::is_unsigned_v<T>);
+    //static_assert(boost::range_iterator<RangeT>::type);
+    using IteratorT = typename boost::range_iterator<RangeT>::type;
+    using DerefIteratorT = decltype(*std::declval<IteratorT>());
+    using RemoveCvRefDerefIteratorT = std::remove_cv_t<std::remove_reference_t<DerefIteratorT>>; //  std::remove_cvref_t<DerefIteratorT>
+    static_assert(std::is_same_v<RemoveCvRefDerefIteratorT, bool>);
+    [[maybe_unused]] unsigned n_all_bits = boost::size(r);
+    assert(n_all_bits < 8 * sizeof(T));
+    T mask = static_cast<T>(1u);
+    T result = static_cast<T>(0u);
+    for (bool b : r) {
+        if (b) {
+            result |= mask;
+        };
+        mask <<= 1;
+    }
     return result;
 }
 
--- app/kstate_op_integral_tests/src/op_integral_bits_tests.cpp
+++ app/kstate_op_integral_tests/src/op_integral_bits_tests.cpp
@@ -1,7 +1,10 @@
 #include <kstate_op_integral/op_integral_bits.hpp>
 
+#include <boost/range.hpp>
+
 #include <gtest/gtest.h>
 
+#include <array>
 #include <cstdint> //for types like: uint64_t
 
 TEST(KstateOpBits, ExtractBit) {
@@ -17,9 +20,44 @@ TEST(KstateOpBits, ExtractBit) {
     EXPECT_TRUE(kstate_op_integral::extract_bit<uint64_t>(0b1100101110, 9));
 }
 
-TEST(ExtensionBoostAdaptorsRotated, FilledRawArray) {
+TEST(KstateOpBits, Rotated) {
     ASSERT_EQ(kstate_op_integral::rotate<uint64_t>(0b1100101110, 10, 0), 0b1100101110);
     ASSERT_EQ(kstate_op_integral::rotate<uint64_t>(0b1100101110, 10, 1), 0b0110010111);
     ASSERT_EQ(kstate_op_integral::rotate<uint64_t>(0b1100101110, 10, 2), 0b1011001011);
     ASSERT_EQ(kstate_op_integral::rotate<uint64_t>(0b1100101110, 10, 9), 0b1001011101);
 }
+
+TEST(KstateOpBits, Refined) {
+    ASSERT_EQ(kstate_op_integral::refine<uint64_t>(0b1100101110, false, 3), 0b1100100110);
+    ASSERT_EQ(kstate_op_integral::refine<uint64_t>(0b1100101110, true, 3), 0b1100101110);
+    ASSERT_EQ(kstate_op_integral::refine<uint64_t>(0b1100101110, false, 4), 0b1100101110);
+    ASSERT_EQ(kstate_op_integral::refine<uint64_t>(0b1100101110, true, 4), 0b1100111110);
+}
+
+TEST(KstateOpBits, ToBitsRange) {
+    const auto r = kstate_op_integral::integral_to_bits_range<uint64_t>(0b1100101110, 10);
+    ASSERT_EQ(boost::size(r), 10);
+    ASSERT_EQ(*std::next(std::begin(r), 0), false);
+    ASSERT_EQ(*std::next(std::begin(r), 1), true);
+    ASSERT_EQ(*std::next(std::begin(r), 2), true);
+    ASSERT_EQ(*std::next(std::begin(r), 3), true);
+    ASSERT_EQ(*std::next(std::begin(r), 4), false);
+    ASSERT_EQ(*std::next(std::begin(r), 5), true);
+    ASSERT_EQ(*std::next(std::begin(r), 6), false);
+    ASSERT_EQ(*std::next(std::begin(r), 7), false);
+    ASSERT_EQ(*std::next(std::begin(r), 8), true);
+    ASSERT_EQ(*std::next(std::begin(r), 9), true);
+}
+
+TEST(KstateOpBits, FromBitsRange) {
+    using BufferT = typename std::array<bool, 10>;
+    //using IteratorT = typename BufferT::iterator;
+    //using ConstIteratorT = typename BufferT::const_iterator;
+    //using RangeT = typename boost::iterator_range<IteratorT>;
+    //using ConstRangeT = typename boost::iterator_range<ConstIteratorT>;
+    BufferT a {false, true, true, true, false, true, false, false, true, true};
+    const auto n = kstate_op_integral::integral_from_bits_range<uint64_t>(a);
+    ASSERT_EQ(n, 0b1100101110);
+}
+
+
