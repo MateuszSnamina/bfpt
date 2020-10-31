c0e58c268d5347b0f0267d696a58fd33dbc85d34
snaminm <mateusz.snamina@gmail.com>
2020-10-31 23:49:56 +0100

    devel

--- app/kstate_impl_tests/src/kstate_concrete_integral_tests.cpp
+++ app/kstate_impl_tests/src/kstate_concrete_integral_tests.cpp
@@ -149,84 +149,15 @@ TEST(DynamicIntegralKstate, ConstructorFromRange8) {
     EXPECT_TRUE(boost::equal(k1.to_range(), v1));
 }
 
-
-//TEST(DynamicIntegralKstate, CompareEqualityTest0) {
-//    using SiteStateTrait = kstate_trait::TraitSiteState<int>;
-//    using Kstate = kstate_impl::DynamicIntegralKstate<SiteStateTrait>;
-//    const std::array<int, 6> v1 = {11, 12, 13, 14, 15, 16};
-//    const std::array<int, 6> v2 = {13, 14, 15, 16, 11, 12};
-//    const Kstate k1(v1, ctr_from_range);
-//    const Kstate k2(v2, ctr_from_range);
-//    EXPECT_TRUE(k1.compare_equality_range(k1.to_range()));
-//    EXPECT_FALSE(k1.compare_equality_range(k2.to_range()));
-//    // ---
-//    EXPECT_TRUE(k1.compare_equality_any_range(k1.to_range()));
-//    EXPECT_FALSE(k1.compare_equality_any_range(k2.to_range()));
-//    // ---
-//    EXPECT_TRUE(k1.compare_equality_kstate(k1));
-//    EXPECT_FALSE(k1.compare_equality_kstate(k2));
-//}
-
-//TEST(DynamicIntegralKstate, CompareTranlationalEqualityTest0) {
-//    using SiteStateTrait = kstate_trait::TraitSiteState<int>;
-//    using Kstate = kstate_impl::DynamicIntegralKstate<SiteStateTrait>;
-//    const std::array<int, 6> v1 = {11, 12, 13, 14, 15, 16};
-//    const std::array<int, 6> v2 = {13, 14, 15, 16, 11, 12};
-//    const std::array<int, 6> v11 = {16, 11, 12, 13, 14, 15};
-//    const std::array<int, 6> v12 = {15, 16, 11, 12, 13, 14};
-//    const std::array<int, 6> v13 = {14, 15, 16, 11, 12, 13};
-//    const std::array<int, 6> v14 = {13, 14, 15, 16, 11, 12};
-//    const std::array<int, 6> v15 = {12, 13, 14, 15, 16, 11};
-//    const Kstate k1(v1, ctr_from_range);
-//    const Kstate k2(v2, ctr_from_range);
-//    const Kstate k11(v11, ctr_from_range);
-//    const Kstate k12(v12, ctr_from_range);
-//    const Kstate k13(v13, ctr_from_range);
-//    const Kstate k14(v14, ctr_from_range);
-//    const Kstate k15(v15, ctr_from_range);
-//    // ---
-//    //ASSERT_FALSE(k1.compare_translational_equality_range(v2)); //TODO rethink!
-//    ASSERT_TRUE(k1.compare_translational_equality_range(v1));
-//    ASSERT_EQ(*k1.compare_translational_equality_range(v1), 0);
-//    ASSERT_TRUE(k1.compare_translational_equality_range(v11));
-//    ASSERT_EQ(*k1.compare_translational_equality_range(v11), 1);
-//    ASSERT_TRUE(k1.compare_translational_equality_range(v12));
-//    ASSERT_EQ(*k1.compare_translational_equality_range(v12), 2);
-//    ASSERT_TRUE(k1.compare_translational_equality_range(v13));
-//    ASSERT_EQ(*k1.compare_translational_equality_range(v13), 3);
-//    ASSERT_TRUE(k1.compare_translational_equality_range(v14));
-//    ASSERT_EQ(*k1.compare_translational_equality_range(v14), 4);
-//    ASSERT_TRUE(k1.compare_translational_equality_range(v15));
-//    ASSERT_EQ(*k1.compare_translational_equality_range(v15), 5);
-//    // ---
-//    //ASSERT_FALSE(k1.compare_translational_equality_range(v2)); //TODO rethink!
-//    ASSERT_TRUE(k1.compare_translational_equality_any_range(v1));
-//    ASSERT_EQ(*k1.compare_translational_equality_any_range(v1), 0);
-//    ASSERT_TRUE(k1.compare_translational_equality_any_range(v11));
-//    ASSERT_EQ(*k1.compare_translational_equality_any_range(v11), 1);
-//    ASSERT_TRUE(k1.compare_translational_equality_any_range(v12));
-//    ASSERT_EQ(*k1.compare_translational_equality_any_range(v12), 2);
-//    ASSERT_TRUE(k1.compare_translational_equality_any_range(v13));
-//    ASSERT_EQ(*k1.compare_translational_equality_any_range(v13), 3);
-//    ASSERT_TRUE(k1.compare_translational_equality_any_range(v14));
-//    ASSERT_EQ(*k1.compare_translational_equality_any_range(v14), 4);
-//    ASSERT_TRUE(k1.compare_translational_equality_any_range(v15));
-//    ASSERT_EQ(*k1.compare_translational_equality_any_range(v15), 5);
-//    // ---
-//    //ASSERT_FALSE(k1.compare_translational_equality_range(k2)); //TODO rethink!
-//    ASSERT_TRUE(k1.compare_translational_equality_kstate(k1));
-//    ASSERT_EQ(*k1.compare_translational_equality_kstate(k1), 0);
-//    ASSERT_TRUE(k1.compare_translational_equality_kstate(k11));
-//    ASSERT_EQ(*k1.compare_translational_equality_kstate(k11), 1);
-//    ASSERT_TRUE(k1.compare_translational_equality_kstate(k12));
-//    ASSERT_EQ(*k1.compare_translational_equality_kstate(k12), 2);
-//    ASSERT_TRUE(k1.compare_translational_equality_kstate(k13));
-//    ASSERT_EQ(*k1.compare_translational_equality_kstate(k13), 3);
-//    ASSERT_TRUE(k1.compare_translational_equality_kstate(k14));
-//    ASSERT_EQ(*k1.compare_translational_equality_kstate(k14), 4);
-//    ASSERT_TRUE(k1.compare_translational_equality_kstate(k15));
-//    ASSERT_EQ(*k1.compare_translational_equality_kstate(k15), 5);
-//}
+TEST(DynamicIntegralKstate, CompareEqualityTest0) {
+    using Kstate = kstate_impl::DynamicIntegral64Kstate<MySiteStateTrait, 3>;
+    const std::array<MySiteState, 6> v1 = {MySiteState(11), MySiteState(12), MySiteState(13), MySiteState(14), MySiteState(14), MySiteState(11)};
+    const std::array<MySiteState, 6> v2 = {MySiteState(13), MySiteState(14), MySiteState(14), MySiteState(11), MySiteState(11), MySiteState(12)};
+    const Kstate k1(v1, ctr_from_range);
+    const Kstate k2(v2, ctr_from_range);
+    EXPECT_TRUE(k1.compare_equality_range(k1.to_range()));
+    EXPECT_FALSE(k1.compare_equality_range(k2.to_range()));
+}
 
 TEST(DynamicIntegralKstate, LeastReplicationShiftTest0) {
     using Kstate = kstate_impl::DynamicIntegral64Kstate<MySiteStateTrait, 3>;
@@ -263,42 +194,6 @@ TEST(DynamicIntegralKstate, LeastReplicationShiftTest3) {
     ASSERT_TRUE(boost::equal(k1.to_range(), v1));
     EXPECT_EQ(k1.n_least_replication_shift(), 6);
 }
-
-//TEST(DynamicIntegralKstate, LeastReplicationShiftTest0) {
-//    using SiteStateTrait = kstate_trait::TraitSiteState<int>;
-//    using Kstate = kstate_impl::DynamicIntegralKstate<SiteStateTrait>;
-//    const std::array<int, 6> v1 = {11, 12, 13, 14, 15, 16};
-//    const Kstate k1(v1, ctr_from_range);
-//    EXPECT_EQ(k1.n_least_replication_shift(), 6);
-//    EXPECT_EQ((k1.template Kstate<kstate_trait::TraitSiteState<int>, boost::random_access_traversal_tag>::n_least_replication_shift()), 6);
-//}
-
-//TEST(DynamicIntegralKstate, LeastReplicationShiftTest1) {
-//    using SiteStateTrait = kstate_trait::TraitSiteState<int>;
-//    using Kstate = kstate_impl::DynamicIntegralKstate<SiteStateTrait>;
-//    const std::array<int, 6> v2 = {11, 12, 13, 11, 12, 13};
-//    const Kstate k2(v2, ctr_from_range);
-//    EXPECT_EQ(k2.n_least_replication_shift(), 3);
-//    EXPECT_EQ((k2.template Kstate<kstate_trait::TraitSiteState<int>, boost::random_access_traversal_tag>::n_least_replication_shift()), 3);
-//}
-
-//TEST(DynamicIntegralKstate, LeastReplicationShiftTest2) {
-//    using SiteStateTrait = kstate_trait::TraitSiteState<int>;
-//    using Kstate = kstate_impl::DynamicIntegralKstate<SiteStateTrait>;
-//    const std::array<int, 6> v3 = {11, 12, 11, 12, 11, 12};
-//    const Kstate k3(v3, ctr_from_range);
-//    EXPECT_EQ(k3.n_least_replication_shift(), 2);
-//    EXPECT_EQ((k3.template Kstate<kstate_trait::TraitSiteState<int>, boost::random_access_traversal_tag>::n_least_replication_shift()), 2);
-//}
-
-//TEST(DynamicIntegralKstate, LeastReplicationShiftTest3) {
-//    using SiteStateTrait = kstate_trait::TraitSiteState<int>;
-//    using Kstate = kstate_impl::DynamicIntegralKstate<SiteStateTrait>;
-//    const std::array<int, 6> v4 = {11, 11, 11, 11, 11, 11};
-//    const Kstate k4(v4, ctr_from_range);
-//    EXPECT_EQ(k4.n_least_replication_shift(), 1);
-//    EXPECT_EQ((k4.template Kstate<kstate_trait::TraitSiteState<int>, boost::random_access_traversal_tag>::n_least_replication_shift()), 1);
-//}
 
 ////---
 //TEST(DynamicIntegralKstate, NormFactor0) {
