d99d44ffa12e90ff5ba113c57fa14a34bcd9bef4
snaminm <mateusz.snamina@gmail.com>
2020-11-07 23:01:16 +0100

    devel

---
+++ app/monostar_hamiltonians/include/monostar_hamiltonians/hamiltonian_params_fo_to_classic_energy_function.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include<monostar_hamiltonians/hamiltonian_params_fo.hpp>
+#include<monostar_hamiltonians/hamiltonian_params_fo_helpers.hpp>
+
+// #######################################################################
+// ## hamiltonian_params_fo_to_classic_energy_function                  ##
+// #######################################################################
+
+namespace monostar_hamiltonians {
+
+monostar_hamiltonians::AcosPlusBsinPlusCsqcosPlusZ hamiltonian_params_fo_to_classic_energy_function(HamiltonianParamsFo params);
+
+}  // end of namespace monostar_hamiltonians
---
+++ app/monostar_hamiltonians/src/hamiltonian_params_fo_to_classic_energy_function.cpp
@@ -0,0 +1,32 @@
+#include<monostar_hamiltonians/hamiltonian_params_fo_to_classic_energy_function.hpp>
+
+// #######################################################################
+// ## hamiltonian_params_fo_to_classic_energy_function                  ##
+// #######################################################################
+
+namespace monostar_hamiltonians {
+
+AcosPlusBsinPlusCsqcosPlusZ hamiltonian_params_fo_to_classic_energy_function(HamiltonianParamsFo params) {
+    // E(θ) = + A*cos(θ) + B*sin(θ)  + C*cos⁴(θ/2) + 2*D*cos²(θ/2)sin²(θ/2) + E*sin⁴(θ/2)
+    //      = + A*cos(θ) + B*sin(θ)  + C[½+½cos(θ)]² + 2*D[½*sin(θ)]² + E[½-½cos(θ)]²
+    //      = + A*cos(θ) + B*sin(θ)  + ¼C[1+cos(θ)]² + ½D[sin(θ)]² + ¼E[1-cos(θ)]²
+    //      = + A*cos(θ) + B*sin(θ)  + (¼C+¼E) + ½(C-E)*cos(θ) + ¼(C+E)*cos²(θ) + ½D - ½D*cos²(θ)
+    //      =  [A+½(C-E)]*cos(θ) - B*sin(θ) + ¼(C+E-2D)*cos²(θ) + ¼(C+E+2D)
+    // where: A ≡ +tau_z_coef,
+    //        B ≡ -tau_minus_coef,
+    //        C ≡ +Pzz_coef,
+    //        D ≡ +Pxz_coef,
+    //        E ≡ +Pxx_coef.
+    const double cos_coef = params.get_tau_z_coef() + 0.5 * (+params.get_Pzz_coef() - params.get_Pxx_coef());
+    const double sin_coef = -params.get_tau_minus_coef();
+    const double sqcos_coef = 0.25 * (params.get_Pzz_coef() + params.get_Pxx_coef() - 2 * params.get_Pxz_coef());
+    const double free_coef = 0.25 * (params.get_Pzz_coef() + params.get_Pxx_coef() + 2 * params.get_Pxz_coef());
+    return AcosPlusBsinPlusCsqcosPlusZ::Builder()
+        .set_cos_coef(cos_coef)
+        .set_sin_coef(sin_coef)
+        .set_sqcos_coef(sqcos_coef)
+        .set_free_coef(free_coef)
+        .build();
+}
+
+}  // end of namespace monostar_hamiltonians
